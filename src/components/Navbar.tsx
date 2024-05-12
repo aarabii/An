270b736c26173988e41444999e9efa4b932fa5b5
losier <arv95464@gmail.com>
2024-05-12 21:38:50 +0530

    added navbar and some logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-import logo from "../assets/logo/logo512.png";
+import logo from "../assets/logo/an_white_bg_removed.png";
 
 import { FaSquareGithub } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa6";
@@ -12,7 +12,7 @@ export const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <Image src={logo} alt="Aarab Nishchal's logo" />
+        <Image className="x-2 w-10"  src={logo} alt="Aarab Nishchal's logo" />
       </div>
 
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -4,9 +4,12 @@ import { Navbar } from "@/components/Navbar";
 
 export const Main = () => {
   return (
-    <React.Fragment>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-300">
+      <div className="top-0 fixed -z-10 h-full w-full"></div>
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-      <Navbar />
-    </React.Fragment>
+      <div className="mx-auto container px-8">
+        <Navbar />
+      </div>
+    </div>
   );
 };
