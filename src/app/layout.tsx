8e0cb326e4029017d376df74483b6cf38e9f8e5f
losier <arv95464@gmail.com>
2024-05-18 20:26:07 +0530

    added some metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,27 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Aarab Nishchal",
+  title: {
+    template: "%s | Aarab Nishchal",
+    default: "Aarab Nishchal",
+  },
   description: "My space on web.",
   icons: [],
+  referrer: "origin-when-cross-origin",
+  metadataBase: new URL("https://aarab.vercel.app"),
+  keywords: [
+    "Aarab",
+    "nishchal",
+    "aarab nishchal",
+    "react",
+    "reactJs",
+    "react portfolio",
+    "portfolio",
+    "portfolio website",
+    "losier",
+    "twexy",
+    "caya",
+  ],
 };
 
 export default function RootLayout({
