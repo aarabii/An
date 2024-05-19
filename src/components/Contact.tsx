2edaecfba9dbd725f9d1aa43f90bb0e8492fd0b1
losier <arv95464@gmail.com>
2024-05-19 22:34:58 +0530

    will add better contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,13 @@
-import data from '@/constants/details.json'
-import { Title } from './common/Title'
+import data from "@/constants/details.json";
+import { Title } from "./common/Title";
+
 
 export const Contact = () => {
   return (
-    <section id='contact'>
-        <Title title='Get in Touch' />
+    <section id="contact">
+      <Title title="Get in Touch" />
 
-        <div className='text-center tracking-tighter'>
-            <p className='my-4'>For any inquiries, please contact me at:</p>
-            <a href={`mailto:${data.email}`} className='border-b'>{data.email}</a> 
-        </div>
+      
     </section>
-  )
-}
+  );
+};
