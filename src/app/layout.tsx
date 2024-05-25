48c49d01713f42d533852926a390715a15a3b05d
losier <arv95464@gmail.com>
2024-05-25 22:56:09 +0530

    changed some metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,11 +53,6 @@ export const metadata: Metadata = {
     description: data.desc,
     images: "https://aarab.vercel.app/logo.svg",
   },
-  viewport: "width=device-width, initial-scale=1",
-  themeColor: "#000000",
-  colorScheme: "dark",
-  manifest: "/manifest.json",
-  robots: "index, follow",
 };
 
 export default function RootLayout({
@@ -69,6 +64,11 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <Head>
         <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />        
         <link rel="canonical" href="https://aarab.vercel.app" />
         <link rel="author" href="/humans.txt" />
         <link rel="security" href="/.well-known/security.txt" />
