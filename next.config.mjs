28df62d2b9131f97d1a3cfe06b4dccdcd2da38ba
losier <arv95464@gmail.com>
2024-05-25 22:06:18 +0530

    added some redirects to socials

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,54 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://www.github.com/losier",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/losier",
+        permanent: true,
+      },
+      {
+        source: "/twitter",
+        destination: "https://www.twitter.com/losier",
+        permanent: true,
+      },
+      {
+        source: "/instagram",
+        destination: "https://www.instagram.com/losier",
+        permanent: true,
+      },
+      {
+        source: "/facebook",
+        destination: "https://www.facebook.com/zzcwc",
+        permanent: true,
+      },
+      {
+        source: "/telegram",
+        destination: "https://t.me/aarab_ii",
+        permanent: true,
+      },
+      {
+        source: "/snapchat",
+        destination: "https://www.snapchat.com/add/aarab.ii",
+        permanent: true,
+      },
+      {
+        source: "/email",
+        destination: "mailto:aarab.nishchal@gmail.com",
+        permanent: true,
+      },
+      {
+        source: "/resume",
+        destination: "/resume.pdf",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
@@ -12,7 +61,7 @@ const nextConfig = {
     service_key: process.env.EMAILJS_SERVICE_ID,
     template_key: process.env.EMAILJS_TEMPLATE_ID,
     user_key: process.env.EMAILJS_PUBLIC_KEY,
-  }
+  },
 };
 
 export default nextConfig;
