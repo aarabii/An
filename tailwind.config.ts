5fad52e99c462966f306638c700a986950c0f125
losier <arv95464@gmail.com>
2024-06-10 14:08:56 +0530

    added purege in tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,16 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  purge: {
+    enabled: true,
+    content: [
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+      // Or if using `src` directory:
+      "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+  },
   content: [
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
