d6b6bb5bd977e4dd833a9010b891ad4e399a0118
losier <arv95464@gmail.com>
2024-06-10 14:10:16 +0530

    fixed a typo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  purge: {
-    enabled: true,
-    content: [
-      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-
-      // Or if using `src` directory:
-      "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-  },
   content: [
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
